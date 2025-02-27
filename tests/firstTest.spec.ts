b4eba9bcc3b30eb9fb1875955a997d6284e61461
yehor <yehor.dubinnyk@nure.ua>
2025-02-27 19:10:19 +0200

    Initial commit

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,15 @@
+import {test} from '@playwright/test'
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+})
+
+
+test('first test', async ({page}) => {
+    await page.getByText('Form Layouts').click()
+})
+
+test('second test', async ({page}) => {
+    await page.getByText('Datepicker').click()
+})
